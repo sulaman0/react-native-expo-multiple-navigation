c3ee2ebbf5ac95f4a06424186a091ee1ff30a678
Sulaman0 <symikhan70@gmail.com>
2025-05-13 23:35:11 +0500

    Setup navigation.!

--- app/(auth)/index2.tsx
+++ app/(auth)/index2.tsx
@@ -7,7 +7,12 @@ export default function LoginScreen() {
     const navigation: any = useNavigation();
     return (
         <View style={styles.container}>
-            <Text>Login Screen</Text>
+            <Text>Login 02 Screen</Text>
+            <Button
+                title="BACK BUTTON"
+                // onPress={() => navigation.navigate('Main', {screen: 'Home'})}
+                onPress={() => navigation.goBack()}
+            />
             <Button
                 title="Login"
                 // onPress={() => navigation.navigate('Main', {screen: 'Home'})}
