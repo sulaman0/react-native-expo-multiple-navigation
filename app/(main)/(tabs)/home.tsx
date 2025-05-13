7ed8f7e58833a2ae107da4fc8c1205eaf41d4c6a
Sulaman0 <symikhan70@gmail.com>
2025-05-13 23:34:24 +0500

    Setup navigation.!

--- app/(main)/(tabs)/home.tsx
+++ app/(main)/(tabs)/home.tsx
@@ -1,12 +1,27 @@
 import {View, Text, Button, StyleSheet} from 'react-native';
 
+import {DrawerActions, useNavigation as useNavigation2} from "@react-navigation/native";
+
 export default function HomeScreen({navigation}: any) {
+    const navigation2 = useNavigation2();
+
     return (
         <View style={styles.container}>
             <Text>Home Screen</Text>
+
+
             <Button
-                title="Go to Details"
-                onPress={() => navigation.navigate('Details')}
+                title="OPEN DRAWER"
+                onPress={() => navigation2.dispatch(DrawerActions.openDrawer())}
+            />
+
+            <Button
+                title="Go to Details (Main) details screen"
+
+                onPress={() =>
+                    // @ts-ignore
+                    navigation2.navigate('settings')
+                }
             />
         </View>
     );
